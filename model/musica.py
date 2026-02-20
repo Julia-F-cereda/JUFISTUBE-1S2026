619630200e12120514c770cbdf243f40c00f9181
julia cereda <julia.cereda@aluno.senai.br>
2026-02-20 11:24:31 -0300

    COMITY: estamos fazendo o processo de salvar musica no ADMIN

--- model/musica.py
+++ model/musica.py
@@ -13,4 +13,21 @@ def recuperar_musicas():
     #fechando
     conexao.close()      
 
-    return musicas                                                                                                  
+    return musicas         
+
+def salvar_musica( cantor: str, duracao: str, nome: str,  imagem: str, genero: str,) -> bool:
+    """ Esa função serve para salvar as musicas dentro da função, e o bool vai mostrar se deu ou nao certo"""
+    try:
+        conexao, cursor = conectar()
+
+        cursor.execute("""INSERT INTO musica (cantor, duracao, nome, url_imagem, nome_genero) 
+                    VALUES (%s, %s, %s, %s, %s)""", [cantor, duracao, nome, imagem, genero])
+
+        conexao.commit()
+
+      
+        conexao.close()
+
+        return True
+    except:
+        return False
